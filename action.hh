acc1b77f96192e8e0bd4925932af0960e59e1e55
BhavneshJPET <bhavnesh@doctoral.uj.edu.pl>
2026-05-29 18:51:19 +0200

    action.hh

--- action.hh
+++ action.hh
@@ -1,29 +1,3 @@
-//#ifndef ACTION_HH
-//#define ACTION_HH
-
-//#include "G4VUserActionInitialization.hh"
-
-//#include "generator.hh"
-//#include "run.hh"
-//#include "event.hh"
-//#include "stepping.hh"
-
-//class MyActionInitialization : public G4VUserActionInitialization
-
-//{
-//public: 
-//	MyActionInitialization();
-//	~MyActionInitialization();
-//	
-//	virtual void BuildForMaster() const;
-//	virtual void Build() const;
-//};
-
-//#endif
-
-
-//NA22
-
 #ifndef ACTION_HH
 #define ACTION_HH
 #include "G4VUserActionInitialization.hh"

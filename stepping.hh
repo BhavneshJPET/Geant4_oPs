f5416c8c20795ba14ce29216f8fab8373d3677b2
BhavneshJPET <bhavnesh@doctoral.uj.edu.pl>
2026-05-29 18:58:26 +0200

    stepping.hh

--- stepping.hh
+++ stepping.hh
@@ -1,30 +1,3 @@
-//#ifndef STEPPING_HH
-//#define STEPPING_HH
-
-//#include "G4UserSteppingAction.hh"
-//#include "G4Step.hh"
-//#include "G4Gamma.hh"
-//#include "G4Electron.hh"
-
-//#include "Construction.hh"
-//#include "event.hh"
-
-//class MySteppingAction : public G4UserSteppingAction
-//{
-//public:
-//    MySteppingAction(MyEventAction* eventAction);
-//    ~MySteppingAction();
-//    
-//    virtual void UserSteppingAction(const G4Step*);
-//    
-//private:
-//    MyEventAction *fEventAction;
-//};
-
-//#endif
-
-//DEEP
-
 #ifndef STEPPING_HH
 #define STEPPING_HH
 

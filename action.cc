6805f71d09b34deb750878883fde071f5919460f
BhavneshJPET <bhavnesh@doctoral.uj.edu.pl>
2026-05-29 18:50:54 +0200

    action.cc

--- action.cc
+++ action.cc
@@ -1,31 +1,3 @@
-//#include "action.hh"
-
-//MyActionInitialization::MyActionInitialization()
-//{}
-
-//MyActionInitialization::~MyActionInitialization()
-//{}
-
-//void MyActionInitialization::BuildForMaster() const
-//{}
-
-//void MyActionInitialization::Build() const
-//{
-//	MyPrimaryGenerator *generator = new MyPrimaryGenerator();
-//	SetUserAction(generator);
-//	
-//	MyRunAction *runAction = new MyRunAction();
-//	SetUserAction(runAction);
-//	
-//	MyEventAction *eventAction = new MyEventAction(runAction);
-//	SetUserAction(eventAction);
-//	
-//	MySteppingAction *steppingAction = new MySteppingAction(eventAction);
-//	SetUserAction(steppingAction);
-//}
-
-//NA22
-
 #include "action.hh"
 
 MyActionInitialization::MyActionInitialization()

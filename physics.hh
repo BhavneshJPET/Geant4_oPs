7ffb27cb9a34fc71e8ef32f5af199229388d2613
BhavneshJPET <bhavnesh@doctoral.uj.edu.pl>
2026-05-29 18:55:13 +0200

    physics.hh

--- physics.hh
+++ physics.hh
@@ -1,26 +1,3 @@
-//#ifndef PHYSICS_HH
-//#define PHYSICS_HH
-
-//#include "G4VModularPhysicsList.hh"
-//#include "G4EmStandardPhysics.hh"
-//#include "G4OpticalPhysics.hh"
-//#include "G4DecayPhysics.hh"
-
-//class MyPhysicsList : public G4VModularPhysicsList
-//{
-//public:
-//    MyPhysicsList();
-//    ~MyPhysicsList();
-//    
-//    virtual void ConstructParticle();
-//    virtual void ConstructProcess();
-//};
-
-//#endif
-
-
-//DEEP
-
 #ifndef PHYSICS_HH
 #define PHYSICS_HH
 

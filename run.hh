4b6dac131e35443ba6a1577c5826e87b431a803f
BhavneshJPET <bhavnesh@doctoral.uj.edu.pl>
2026-06-01 16:19:08 +0200

    run.hh

--- run.hh
+++ run.hh
@@ -4,12 +4,14 @@
 #include "G4UserRunAction.hh"
 #include "G4Run.hh"
 #include "G4SystemOfUnits.hh"
+#include "G4Accumulable.hh"
+#include "G4AccumulableManager.hh"
 #include <sstream>
 
 class MyRunAction : public G4UserRunAction
 {
 public:
-    MyRunAction();
+    MyRunAction(G4String filename);
     ~MyRunAction();
 
     virtual void BeginOfRunAction(const G4Run*);
@@ -21,11 +23,16 @@ public:
                       G4double totalEnergy,
                       G4double decayZ);
 
+    void AddTrueCoincidence() { fTrueCoincidences += 1; }
+
 private:
-    // Run-level accumulators
-    G4int    fTotal3GammaEvents;  // number of events with exactly 3 decay gammas
-    G4double fTotalEnergySum;     // sum of 3-gamma total energies (for average)
-    G4double fDecayXSum;          // sum of decay X positions (for average)
+    G4String fFilename;
+    // Run-level accumulators (thread-safe for MT)
+    G4Accumulable<G4int>    fTotalEvents;
+    G4Accumulable<G4int>    fTotal3GammaEvents;  // number of events with exactly 3 decay gammas
+    G4Accumulable<G4double> fTotalEnergySum;     // sum of 3-gamma total energies (for average)
+    G4Accumulable<G4double> fDecayXSum;          // sum of decay X positions (for average)
+    G4Accumulable<G4int>    fTrueCoincidences;   // number of events where all 3 gammas hit
 };
 
 #endif

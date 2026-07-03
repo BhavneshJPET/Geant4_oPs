1812b1774b204f34e63b9f7f69d6c0a86e3fc78b
BhavneshJPET <bhavnesh@doctoral.uj.edu.pl>
2026-07-03 09:05:03 +0200

    Update run.hh

--- run.hh
+++ run.hh
@@ -15,24 +15,26 @@ public:
     ~MyRunAction();
 
     virtual void BeginOfRunAction(const G4Run*);
-    virtual void EndOfRunAction(const G4Run*);
+    virtual void EndOfRunAction  (const G4Run*);
 
-    // Called by EventAction::EndOfEventAction for every event.
-    // Accumulates statistics only for genuine 3-gamma o-Ps events.
+    // Called by EventAction::EndOfEventAction for every event
     void AddEventData(G4int gammaCount,
                       G4double totalEnergy,
                       G4double decayZ);
 
-    void AddTrueCoincidence() { fTrueCoincidences += 1; }
+    void AddTrueCoincidence();
+    void Add2GammaEvent();
 
 private:
     G4String fFilename;
+
     // Run-level accumulators (thread-safe for MT)
     G4Accumulable<G4int>    fTotalEvents;
-    G4Accumulable<G4int>    fTotal3GammaEvents;  // number of events with exactly 3 decay gammas
-    G4Accumulable<G4double> fTotalEnergySum;     // sum of 3-gamma total energies (for average)
-    G4Accumulable<G4double> fDecayXSum;          // sum of decay X positions (for average)
-    G4Accumulable<G4int>    fTrueCoincidences;   // number of events where all 3 gammas hit
+    G4Accumulable<G4int>    fTotal3GammaEvents;
+    G4Accumulable<G4int>    fTotal2GammaEvents;  // Pick-off / p-Ps events
+    G4Accumulable<G4double> fTotalEnergySum;
+    G4Accumulable<G4double> fDecayZSum;          // FIXED: Renamed to match Z-axis tracking in implementation
+    G4Accumulable<G4int>    fTrueCoincidences;
 };
 
 #endif
